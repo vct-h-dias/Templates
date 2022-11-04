f9df75b943cca679bdd7969cbc078ed221033a96
vct-h-dias <d.hugo@aluno.ifsp.edu.br>
2022-11-03 22:41:16 -0300

    initial commit

---
+++ index.js
@@ -0,0 +1,32 @@
+function cadastro (nome, matricula, frequencia, nota, turma) {
+    console.log(this);
+    this.nome = nome;
+    this.matricula = matricula;
+    this.frequencia = frequencia;
+    this.nota = nota;
+    this.turma = turma;
+}
+
+var cad = [];
+
+cad[0] = new cadastro("João do Picão", "CP3017311", "75.6", "10" , "EOFOR");
+cad[1] = new cadastro("Vião", "CP301729X", "92.0", "10", "EOELSE");
+
+var listar = (turma) => {
+    
+    var conteudo = "<table class='table'>";
+    conteudo += "<tr><th>Nome</th><th>Matricula</th><th>Turma</th></tr>";
+    for (let i = 0; i < cad.length; i++) {
+        console.log(turma)
+        if (cad[i].turma == turma) {
+            conteudo += "<tr><td>" + cad[i].nome + "</td>";
+            conteudo += "<td>" + cad[i].matricula + "</td>";
+            conteudo += "<td>" + cad[i].turma + "</td></tr>";
+        }
+    }
+    
+    conteudo += "</table>";
+    
+    const divcasa = document.getElementById('list');
+    divcasa.innerHTML = conteudo;
+}

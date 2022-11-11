8fa84c960b61ac6ad6dbfef5157f94169b21ff7e
vct-h-dias <d.hugo@aluno.ifsp.edu.br>
2022-11-10 21:48:09 -0300

    geral update (general code)

--- index.js
+++ index.js
@@ -1,34 +1,44 @@
 class Cadastro {
-    constructor(nome, matricula, frequencia, nota, turma){
+    constructor(nome, genero, autor, avaliacao){
         console.log(this);
-        this.nome = nome;
-        this.matricula = matricula;
-        this.frequencia = frequencia;
-        this.nota = nota;
-        this.turma = turma;
+        this.Nome = nome;
+        this.Genero = genero;
+        this.Autor = autor;
+        this.Avaliacao = avaliacao;
     }
 }
 
 var cad = [];
 
-cad[0] = new Cadastro("João do Picão", "CP3017311", "75.6", "10" , "EOFOR");
-cad[1] = new Cadastro("Vião", "CP301729X", "92.0", "10", "EOELSE");
+cad[0] = new Cadastro("Harry Potter", "Romance", "JK", "10");
+cad[1] = new Cadastro("Dom Quixote", "Romance", "Miguel de Cervantes", "9");
 
 var listar = (turma) => {
     
     var conteudo = "<table class='table'>";
-    conteudo += "<tr><th>Nome</th><th>Matricula</th><th>Turma</th></tr>";
+    conteudo += "<tr><th>Nome</th><th>Gênero</th><th>Autor</th><th>Avaliação</th></tr>";
     for (let i = 0; i < cad.length; i++) {
         console.log(turma)
-        if (cad[i].turma == turma) {
-            conteudo += "<tr><td>" + cad[i].nome + "</td>";
-            conteudo += "<td>" + cad[i].matricula + "</td>";
-            conteudo += "<td>" + cad[i].turma + "</td></tr>";
-        }
+        conteudo += "<tr><td>" + cad[i].Nome + "</td>";
+        conteudo += "<td>" + cad[i].Genero + "</td>";
+        conteudo += "<td>" + cad[i].Autor + "</td>";
+        conteudo += "<td>" + cad[i].Avaliacao + "</td></tr>";
+        
     }
     
     conteudo += "</table>";
     
     const divcasa = document.getElementById('list');
     divcasa.innerHTML = conteudo;
+}
+
+function matricular(){
+    var l = new Cadastro(nome.value, genero.value, autor.value, avaliacao.value);
+    console.log(l);
+    cad.push(l);
+    nome.value = ""
+    genero.value = ""
+    autor.value = ""
+    avaliacao.value = ""
+    listar();
 }
